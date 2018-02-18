aefc629cef5b5b90c49f453f99581490668a6ed6
Jack Evans <jack@evans.gb.net>
2018-02-18 22:49:36 +0000

    Set Cors header in view

--- main.go
+++ main.go
@@ -69,6 +69,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(resp)
 
 }
